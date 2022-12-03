0b4a167f5f90de1cf994d2855707f151fcb883c5
Taka <okawatakanori9916@gmail.com>
2022-12-04 01:59:03 +0900

    add clearInterval

--- index.js
+++ index.js
@@ -4,10 +4,11 @@
 // 遅延実行
 setTimeout(() => {
   console.log('in the timeout');
+  clearInterval(int); // 繰り返し処理 停止
 }, 3000)
 
 
 // 繰り返し実行
-setInterval(() => {
+const int = setInterval(() => {
   console.log('in the interval');
 },1000)
