a383380cc6767408ec0f7d7fefaae4bbee7bb437
Taka <okawatakanori9916@gmail.com>
2022-12-04 02:05:51 +0900

    __dirname __filenameを表示

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// console.log(global);
+// Global Objectの学習
 
+console.log(global)
 
 // 遅延実行
 setTimeout(() => {
@@ -12,3 +13,10 @@ setTimeout(() => {
 const int = setInterval(() => {
   console.log('in the interval');
 },1000)
+
+
+// 現在のディレクトまでの絶対パスを取得
+console.log(__dirname);
+
+// ファイル名までを取得
+console.log(__filename);
