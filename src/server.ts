a9475811f48b80e06d887005152efc1cb7a4603a
Taka <okawatakanori9916@gmail.com>
2022-12-07 02:30:09 +0900

    serverを立ち上げるところまで、レスポンス処理などは行なっていない

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import * as http from 'http';
+
+const server = http.createServer((req ,res ) => {
+  console.log('request made')
+});
+
+server.listen(3000, 'localhost', () => {
+  console.log("listening for requests on port 3000");
+})
