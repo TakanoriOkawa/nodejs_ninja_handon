087e689020ca5ba882bd032be8603cbb6107314f
Taka <okawatakanori9916@gmail.com>
2022-12-07 22:48:22 +0900

    URLとHTTPメソッドを取得

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import * as http from 'http';
 
 const server = http.createServer((req ,res ) => {
   console.log('request made')
+  console.log(req.url, req.method) // リクエストのURLとHTTPメソッドを取得
 });
 
 server.listen(3000, 'localhost', () => {
