5429dab97bba34a97652d423ad15a10ba4111bf4
Taka <okawatakanori9916@gmail.com>
2022-12-08 02:37:09 +0900

    nodemonでサーバー起動する設定 lodashの導入

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import * as http from 'http';
 import * as fs from 'fs';
 
+import * as _ from 'lodash';
+
 type Request = http.IncomingMessage
 type Response = http.ServerResponse
 
 const server = http.createServer((req: Request ,res: Response ) => {
   console.log(req.url, req.method) // リクエストのURLとHTTPメソッドを取得
 
-  
+  // lodash
+  const num = _.random(0, 20);
+  console.log(num)
   
   // htmlを返却する設定
   res.setHeader('Content-Type', 'text/html');
