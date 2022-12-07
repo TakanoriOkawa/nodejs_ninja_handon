f92de6d5d14423ea8ccba011ab21df42add5917a
Taka <okawatakanori9916@gmail.com>
2022-12-07 23:11:19 +0900

    文字列返却

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,15 @@ import * as http from 'http';
 const server = http.createServer((req ,res ) => {
   console.log('request made')
   console.log(req.url, req.method) // リクエストのURLとHTTPメソッドを取得
+
+  // プレーンなテキストをブラウザに返す設定
+  res.setHeader('Content-Type', 'text/plain');
+
+  // 文字列を返却
+  res.write('hello nodeJs');
+
+  // 終了
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {
