f49f40bd7db2bf79cdae976dab0b6eaf30fd9ab3
Taka <okawatakanori9916@gmail.com>
2022-12-08 02:03:12 +0900

    ルーティング追加

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,33 @@ import * as fs from 'fs';
 type Request = http.IncomingMessage
 type Response = http.ServerResponse
 
-
 const server = http.createServer((req: Request ,res: Response ) => {
   console.log(req.url, req.method) // リクエストのURLとHTTPメソッドを取得
+
+  
   
   // htmlを返却する設定
   res.setHeader('Content-Type', 'text/html');
+
+  // ルーティング設定
+  let path = './public/views/';
+
+  switch(req.url){
+    case '/':
+      path += 'index.html';
+      break;
+    case '/about':
+      path += 'about.html';
+      break;
+    default:
+      path += '404.html';
+      break;
+  }
+
+  console.log(path);
+
   // ルートディレクトリからのパスを指定
-  fs.readFile('./public/views/index.html', (err: any, data: any) => {
+  fs.readFile(path, (err: any, data: any) => {
     if(err) {
       console.log(err);
       res.end();
