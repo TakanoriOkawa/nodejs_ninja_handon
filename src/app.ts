84e07ddb0361407859ebabf352c76d94cd97b7f1
Taka <okawatakanori9916@gmail.com>
2022-12-09 22:25:42 +0900

    expressのルーティングを実装

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+// const express = require('express');
+import * as express from 'express';
+import path = require('path');
+
+const app = express();
+
+app.listen(3000)
+
+const options = {
+  root: __dirname,
+  headers: {
+    'x-timestamp': Date.now()
+  }
+}
+
+app.get('/', (req, res) => {
+  res.set('content-type', 'text/html')
+  // 公式
+  // https://expressjs.com/ja/api.html#res.sendFile
+  // 第二引数でオプションを指定できる。
+  res.sendFile('./views/index.html', options);
+});
+
+
+app.get('/about', (req, res) => {
+  res.sendFile('./views/about.html', options)
+})
