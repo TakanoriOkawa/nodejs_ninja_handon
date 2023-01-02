c030ccab1117cbaf86ed05dcce153551931b2c31
Taka <okawatakanori9916@gmail.com>
2023-01-02 19:58:01 +0900

    愚直に非同期処理を順番に実行した場合の例

--- src/promise.ts
+++ src/promise.ts
@@ -1,1 +1,17 @@
 console.log("value!");
+
+// 非同期処理が複数あった場合、それらを連続して実行したいとすると以下のようなコードになる
+function sleep(cb: (val:number) => void, val: number) {
+  setTimeout(() => {
+    console.log(val++)
+    cb(val);
+  },1000)
+}
+
+sleep(function(val:number) {
+  sleep(function(val:number) {
+    sleep(function(val:number) {
+
+    },val)
+  },val)
+},0);
